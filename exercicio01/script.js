2b17effe4828d1c929ee3da06478cb2c7e350780
Diogo <diogo.vpa@gmail.com>
2024-05-13 22:48:12 +0100

    Added files to start working on the exercise

---
+++ exercicio01/script.js
@@ -0,0 +1,6 @@
+
+const paises = ["Albânia", "Bolívia","Canadá", "Dinamarca", "Etiópia", "Finlândia", "Alemanha","Hungria", "Irlanda", "Japão", "Quênia"]
+
+for ( let i = 0; 0 <= paises.length; i++){
+    console.log(paises[i++]);
+}
