59353c1bd5943afc97227c765d559694dc5728cd
DiogoMM <diogo.vpa@gmail.com>
2024-05-14 23:22:28 +0100

    Finished all exercises

---
+++ exercicio03/script.js
@@ -0,0 +1,8 @@
+const titulo = "Isto é um título longo de um livro";
+console.log (titulo.length);
+
+const mudaTitulo = titulo.replace("livro", "diário");
+
+const tituloMais = titulo + "mas não faz mal porque não é uma seca";
+
+console.log(titulo.slice(0, 4));
