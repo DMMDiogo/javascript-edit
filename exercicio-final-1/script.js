4adf35174acac697b0de394ea6a5b5da9addac60
DiogoMM <diogo.vpa@gmail.com>
2024-06-08 22:49:46 +0100

    Finished final exercise one

--- exercicio-final-1/script.js
+++ exercicio-final-1/script.js
@@ -2,8 +2,6 @@ class Ecommerce {
     constructor() {
         this.products = []; 
         this.cart = []; 
-        this.productld = productld;
-        this.price = price;
     }
 
     
@@ -13,21 +11,70 @@ class Ecommerce {
       
     }
 
-    setProductPrice(productld, price) {
+    setProductPrice(productId, newPrice) {
+        this.products.forEach((product) => { 
+            if (product.id === productId) {
+                product.price = newPrice;
+            }
+        });
+    }
 
-        this.products.filter(() => { })
+    getAllProducts() {
+        return this.products;
+    }
 
+    getAllProductsName(){
+        return this.products.map((product) => {return product.name}).join(";");
+    }
+
+    getProductById (id) {
+        return this.products.filter((product) => {return product.id == id});
+       
+    }
+
+    getProductByName (name) {
+        const gpbn = this.products.filter((product) => { return product.name == name});
+        console.log(gpbn);
+    }
+
+    getProductsByPrice(initialPrice, finalPrice) {
+        return this.products.filter((product) => {
+            return product.price >= initialPrice && product.price <= finalPrice;
+        });
+       
+    }
+
+    addProductToCart (product) {
+        return this.cart.push(product); 
+    }
+
+    getCart () {
+        return this.cart;
+    }
+
+    getCartTotalPrice () {
+        return this.products.reduce((total, product) => total + product.price,
+    0, )
     }
 }
 
 
 const ecommerce = new Ecommerce();
 
-ecommerce.addProduct({ name: "Product 1", price: 10 });
-ecommerce.addProduct({ name: "Product 2", price: 20 });
-ecommerce.addProduct({ name: "Product 3", price: 30 });
-
-
+ecommerce.addProduct({ name: "Product 1", price: 10, id:1 });
+ecommerce.addProduct({ name: "Product 2", price: 20, id:2 });
+ecommerce.addProduct({ name: "Product 3", price: 30, id:3});
+ecommerce.setProductPrice("Product 1", "5")
+ecommerce.addProductToCart(ecommerce.getProductById(1));
+ecommerce.addProductToCart(ecommerce.getProductById(2));
+ecommerce.getAllProducts();
+ecommerce.getProductById(2);
+ecommerce.getProductByName("Product 1");
+ecommerce.getProductsByPrice(0, 25);
 console.log(ecommerce.products);
+console.log(ecommerce.getAllProductsName());
+console.log(ecommerce.getCart());
+console.log(ecommerce.getCartTotalPrice())
+
 
 
