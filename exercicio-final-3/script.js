47463dc004039dccc71b46f0ce6f9ec32cbf9277
DiogoMM <diogo.vpa@gmail.com>
2024-06-29 19:02:23 +0100

    Continued exercise 3

--- exercicio-final-3/script.js
+++ exercicio-final-3/script.js
@@ -2,7 +2,7 @@ const loadProducts = async () => {
     const response = await fetch("https://fakestoreapi.com/products/1");
     const info = await response.json();
 
-console.log(info)
+
     const createProducts = () => {
         const div = document.createElement("div");
         document.body.appendChild(div);
@@ -19,6 +19,26 @@ console.log(info)
 
 loadProducts()
 
+const loadRelatedProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products/category/jewelery");
+    const info = await response.json();
+
+console.log(info)
+
+info.forEach(element => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+
+    const title = document.createElement("h3");
+    title.textContent = element.title;
+    div.appendChild(title);
+
+    const price = document.createElement("p");
+    price.textContent = element.price; 
+    div.appendChild(price);
+});
+    
+}
 
 
-
+loadRelatedProducts()
